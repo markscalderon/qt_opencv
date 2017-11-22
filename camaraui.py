0db5b894f0a9e661462708656849a0606c7677ce
Marks Calderon <Marks C>
2017-11-22 12:42:17 -0500

    first commit

---
+++ camaraui.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'camara_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import cv2
+import numpy as np
+import threading
+import time
+import Queue
+
+running = False
+capture_thread = None
+q = Queue.Queue()
+
+
+def camera(cam, queue, width, height , fps):
+    global running
+    capture = cv2.VideoCapture(cam)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    capture.set(cv2.CAP_PROP_FPS, fps)
+
+    while(running):
+        frame = {}
+        ret , img = capture.read()
+        frame["img"] = img
+        if queue.qsize() < 10:
+            queue.put(frame)
+        else:
+            print(queue.qsize())
+class OwnImageWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(OwnImageWidget, self).__init__(parent)
+        self.image = None
+
+    def setImage(self, image):
+        self.image = image
+        sz = image.size()
+        self.setMinimumSize(sz)
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        if self.image:
+            qp.drawImage(QtCore.QPoint(0, 0), self.image)
+        qp.end()
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(789, 524)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(30, 40, 741, 421))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.startButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.startButton.setObjectName("startButton")
+        self.verticalLayout.addWidget(self.startButton)
+        self.groupBox = QtWidgets.QGroupBox(self.verticalLayoutWidget)
+        self.groupBox.setObjectName("groupBox")
+        self.widget = QtWidgets.QWidget(self.groupBox)
+        self.widget.setGeometry(QtCore.QRect(10, 10, 711, 441))
+        self.widget.setObjectName("widget")
+        self.verticalLayout.addWidget(self.groupBox)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 789, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+        self.startButton.clicked.connect(self.start_clicked)
+
+        self.window_width = self.widget.frameSize().width()
+        self.window_height = self.widget.frameSize().height()
+        self.ImgWidget = OwnImageWidget(self.widget)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(1)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.startButton.setText(_translate("MainWindow", "Start camera"))
+        self.groupBox.setTitle(_translate("MainWindow", "Streaming"))
+
+    def start_clicked(self):
+        global running
+        running = True
+        capture_thread.start()
+        self.startButton.setEnabled(False)
+        self.startButton.setText('Starting ....')
+
+    def update_frame(self):
+        if not q.empty():
+            self.startButton.setText('Camera is live')
+            frame = q.get()
+            img = frame["img"]
+
+            img_height, img_width, img_colors = img.shape
+            scale_w = float(self.window_width) / float(img_width)
+            scale_h = float(self.window_height) / float(img_height)
+            scale = min([scale_w, scale_h])
+
+            if scale == 0:
+                scale = 1
+
+            img = cv2.resize(img, None, fx=scale, fy=scale, interpolation = cv2.INTER_CUBIC)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            height, width, bpc = img.shape
+            bpl = bpc * width
+            image = QtGui.QImage(img.data, width, height, bpl, QtGui.QImage.Format_RGB888)
+            self.ImgWidget.setImage(image)
+
+    def closeEvent(self, event):
+        global running
+        running = False
+
+
+if __name__ == "__main__":
+    import sys
+    capture_thread = threading.Thread(target=camera, args = (0, q, 640, 480, 30))
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
